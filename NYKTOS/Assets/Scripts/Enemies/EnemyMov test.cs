6675082c9721f6313a5ce7e25255b1a2d6c8478c
MaEduardaB <125443437+DanielZhu26@users.noreply.github.com>
2024-02-21 09:59:46 +0100

    Arreglo de la anim del vespertilio y Script aux del movimiento de los vespertilios

--- NYKTOS/Assets/Scripts/Enemies/EnemyMov test.cs
+++ NYKTOS/Assets/Scripts/Enemies/EnemyMov test.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class EnemyMovtest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform _playertransfom;
+    private float speed = 2f;
 
+   
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, _playertransfom.transform.position, speed * Time.deltaTime);
+
     }
+
 }
