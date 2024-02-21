de86730735cd06b1d4d578b1d4452725546009c2
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-02-21 13:22:32 +0100

    changes

--- NYKTOS/Assets/Scripts/MovementSystem/Blink/BlinkHitbox.cs
+++ NYKTOS/Assets/Scripts/MovementSystem/Blink/BlinkHitbox.cs
@@ -12,10 +12,10 @@ public class BlinkHitbox : MonoBehaviour
     #endregion
 
     public bool isColliding = false;
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         //el numero de capa de whatLayerToDetect, es decir, TerrainCollision, es 3. OutOfBounds es 6.
-        if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
             isColliding = true;
             renderer.color = Color.red;
@@ -24,7 +24,7 @@ public class BlinkHitbox : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
             isColliding = false;
             renderer.color = Color.green;
