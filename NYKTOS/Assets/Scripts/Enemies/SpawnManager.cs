9763a7f5310e149a59b9c0bf32a84b6a87ff4ab4
MaEduardaB <mbeckers@ucm.es>
2024-02-23 20:38:14 +0100

    Spawner en el turorial

--- NYKTOS/Assets/Scripts/Enemies/SpawnManager.cs
+++ NYKTOS/Assets/Scripts/Enemies/SpawnManager.cs
@@ -9,22 +9,21 @@ public class SpawnManager : MonoBehaviour
     //  Codigo de Maria :p
     #region references
     private EnemySpawner _enemySpawner;
-    //[SerializeField] private Text _waveText;
+    
     #endregion
 
     #region methods
     public void SpawnNextWave() {
-        StartCoroutine(WaveTextEnableRoutine()); //lo llame asi pq estaba probando con texto
+        StartCoroutine(EnableRoutine()); 
     }
 
-    private IEnumerator WaveTextEnableRoutine() {
+    private IEnumerator EnableRoutine() {
 
-        //_waveText.text = "Wave " + _enemySpawner.GetWaveNumber();
-        //_waveText.gameObject.SetActive(true);
+        
         _enemySpawner.EnableNextWaveSpawning();
 
         yield return new WaitForSeconds(3f);
-        //_waveText.gameObject.SetActive(false);
+      
     }
 
     #endregion
@@ -34,7 +33,7 @@ public class SpawnManager : MonoBehaviour
     {
         _enemySpawner = GetComponent<EnemySpawner>();
         _enemySpawner.StartGameSpawning();
-        //_waveText.text = "Wave " + 00;
+        
     }
 
     // Update is called once per frame
