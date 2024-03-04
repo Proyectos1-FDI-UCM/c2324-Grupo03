6b2ef18e200e7f21fd7a89b1500bc178da882e51
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-03-04 13:55:45 +0100

    probando navmesh en mi escena, NO TOCAR

---
+++ NYKTOS/Assets/Scripts/IA/Behaviours/PruebaBehaviours/MoveToPlayerBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToPlayerBehaviour : MonoBehaviour, IBehaviour
+{
+    private NavMeshPath _path;
+    private Vector2 _position = Vector2.zero;
+    private Transform _myTransform;
+    
+    public void PerformBehaviour()
+    {
+        _path = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, _position, NavMesh.AllAreas,_path);
+
+        for (int i =0; i<_path.corners.Length - 1; i++)
+        {
+            Debug.DrawLine(_path.corners[i], _path.corners[i+1]);
+        }
+        
+    }
+
+    private void Start()
+    {
+        _myTransform = transform;
+    }
+}
