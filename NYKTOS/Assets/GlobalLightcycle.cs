3f742eb601bbc4467ea883c9b3476ff812c7d121
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2024-03-06 10:35:22 +0100

    Global light cycle

---
+++ NYKTOS/Assets/GlobalLightcycle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Light2D))]
+public class GlobalLightcycle : MonoBehaviour
+{
+
+    private Light2D _globalLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _globalLight = GetComponent<Light2D>();
+
+        GameManager.Instance.GameStateChanged.AddListener(GlobalLightSwitch);
+
+        _globalLight.intensity = (GameManager.Instance.State == GameState.Day) ? 1 : 0;
+    }
+
+    void GlobalLightSwitch(GameState state)
+    {
+        if(state == GameState.Night)
+        {
+            _globalLight.intensity = 0;
+        }
+        else if (state == GameState.Day)
+        {
+            _globalLight.intensity = 1;
+        }
+    }
+}
