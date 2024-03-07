167d7f64cd697ca3d45884531eda746145e70f8d
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-03-08 00:01:26 +0100

    Fixed player revive (again)

--- NYKTOS/Assets/Scripts/Environment/Nexus/NexusComponent.cs
+++ NYKTOS/Assets/Scripts/Environment/Nexus/NexusComponent.cs
@@ -9,13 +9,16 @@ public class NexusComponent : MonoBehaviour, IBuilding
     private int _nightLength = 180;
 
     [SerializeField]
-    private PlayerDeath _player;
+    private GameObject _player;
+
+    private PlayerStateMachine _playerState;
+    private PlayerDeath _playerDeath;
     #endregion
 
     public void OpenMenu()
     {
         if(GameManager.Instance.State == GameState.Day) MenuManager.Instance.OpenMenu(1);
-        else _player.Revive();
+        else if(_playerState.playerState == PlayerState.Dead) _playerDeath.Revive();
     }
 
     public void CloseMenu() => MenuManager.Instance.CloseMenu();
@@ -34,5 +37,8 @@ public class NexusComponent : MonoBehaviour, IBuilding
     private void Start()
     {
         BuildingManager.Instance.AddBuilding(this.gameObject);
+        
+        _playerDeath = _player.GetComponent<PlayerDeath>();
+        _playerState = _player.GetComponent<PlayerStateMachine>();
     }
 }
