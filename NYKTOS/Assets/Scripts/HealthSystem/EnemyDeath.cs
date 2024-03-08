604fa631f8b447884bdf15e3898ad65ced07364e
Marco <marcgo19@ucm.es>
2024-03-08 01:10:17 +0100

    añadido muerte de enemigos al no estar en noche

--- NYKTOS/Assets/Scripts/HealthSystem/EnemyDeath.cs
+++ NYKTOS/Assets/Scripts/HealthSystem/EnemyDeath.cs
@@ -1,19 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 [System.Serializable]
 public class EnemyDeath : MonoBehaviour, IDeath
 {
+    void GameStateListener(GameState state)
+    {
+        if(!(GameState.Night == state))
+        {
+            Death();
+        }
+    }
+
     public void Death()
     {
-        
-        Destroy(gameObject);
+        SpawnManager.Instance.ConcurrentEnemies--;
 
+        Destroy(gameObject);
     }
 
     public void Talk()
     {
         Debug.Log("Soy un enemigo!!");
+    }
+
+    void Start(){
+        GameManager.Instance.GameStateChanged.AddListener(GameStateListener);
     }
 }
