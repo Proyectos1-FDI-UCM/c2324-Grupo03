bb41109061b84ffa715802667ce0619761a0d213
Iker-kun <ikerjuma@ucm.es>
2024-03-17 00:09:47 +0100

    Tenemos torreta (Rotación, balas, colision con enemigos, daño) AAAAAAAAAAAA

---
+++ NYKTOS/Assets/Scripts/Buildings/Turret/TurretShootingComponent.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretShootingComponent : MonoBehaviour
+{
+    private TurretTargetingComponent _turretTargetingComponent;
+    private Vector3 _DirectionToEnemy;
+    [SerializeField]
+    private GameObject _proyectil;
+    private Transform _myTransform;
+    [SerializeField]
+    private float DurationOfBullet = 4f;
+    [SerializeField]
+    private float RechargeTime = 1f;
+    private float RechargeTimePrincipal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _turretTargetingComponent = GetComponent<TurretTargetingComponent>();
+        RechargeTimePrincipal = RechargeTime;
+        _myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _DirectionToEnemy = _turretTargetingComponent.DirectionToEnemy();
+
+        if (_DirectionToEnemy != Vector3.zero)
+        {
+            RechargeTime -= Time.deltaTime;
+
+            if (RechargeTime < 0)
+            {
+                GameObject bullet = Instantiate(_proyectil, _myTransform.position, Quaternion.identity, _myTransform);
+                Destroy(bullet,DurationOfBullet);
+                RechargeTime = RechargeTimePrincipal;
+            }
+            
+        }    
+    }
+
+
+}
