418330c62e462f403d197ac6abfdc42b120b83dc
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2024-03-18 20:25:35 +0100

    Menú terminado (por ahora)

---
+++ NYKTOS/Assets/Scripts/Other/Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void Iniciar()
+    {
+        GameManager.Instance.UpdateGameState(GameState.Day);
+    }
+
+
+}
