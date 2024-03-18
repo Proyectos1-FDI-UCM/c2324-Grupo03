6fceab23b226e5e31e1e9477726f70e803984e62
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-03-18 21:31:00 +0100

    clases abstractas para las hitboxes de da;o. mas comodo todo

--- NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/EnemyWeapons/VespertilioAttackHitbox.cs
+++ NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/EnemyWeapons/VespertilioAttackHitbox.cs
@@ -2,48 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VespertilioAttackHitbox : MonoBehaviour
+public class VespertilioAttackHitbox : WeaponBehaviour
 {
-    #region properties
-    public int attackDamage = 0;
-    public AttackType attackType;
-    #endregion
-
     #region references
     private Transform _myTransform;
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-
-        if (collision.gameObject.TryGetComponent(out HealthComponent health)) //QUITAR VIDA
-        {
-            health.Damage(attackDamage);
-            //Javi ha hecho una corrección a este código para que sea más limpio. Dejo este de ejemplo :)
-        }
-
-        if (collision.gameObject.GetComponentInChildren<IKnockback>() != null)
-        {
-
-            IKnockback[] iknockbackChildren = collision.gameObject.GetComponentsInChildren<IKnockback>();
-
-            for (int i = 0; i < iknockbackChildren.Length; i++)
-            {
-                iknockbackChildren[i].CallKnockback(_myTransform.parent.position);
-            }
-
-        }
-
-        if (attackType == AttackType.Fire && collision.gameObject.TryGetComponent(out SetOnFireDebuff setOnFire)) //DAÑO DE FUEGO
-        {
-            setOnFire.enabled = true;
-        }
-
-        else if (attackType == AttackType.Slow && collision.gameObject.TryGetComponent(out SlowDebuff slow)) //RALENTIZAR
-        {
-            slow.enabled = true;
-        }
+        Damage(collision);
+        Knockback(collision, _myTransform);
     }
 
     private void Awake()
