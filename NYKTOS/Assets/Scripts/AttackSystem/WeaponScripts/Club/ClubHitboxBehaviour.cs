1ec68bc24167122a333cb7c025a3f83691d24a07
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-03-20 18:16:25 +0100

    se ha ido a la poronga la animacion esa fea del garrote

--- NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/Club/ClubHitboxBehaviour.cs
+++ NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/Club/ClubHitboxBehaviour.cs
@@ -4,32 +4,9 @@ using UnityEngine;
 
 public class ClubHitboxBehaviour : WeaponBehaviour
 {
-    #region properties
-    //SE HACE LA ROTACION DESDE EL ANGULO QUE SE LE PASA HASTA EL ANGULO QUE SE LE PASA + ATTACKANGLERANGE
-
-    public float attackAngleRange = 0f; //recorrido que va a hacer, se le pasa en el WeaponClub
-    public float currentAngle = 0f; //angulo en el que se encuentra la hitbox
-    private float maxAngle;
-    public float angleVelocity;
-    #endregion
-
     #region references
     private Transform _myTransform;
     #endregion
-
-    // Update is called once per frame
-    void Update()
-    {
-        currentAngle = currentAngle - angleVelocity * Time.deltaTime;
-        if (currentAngle >= maxAngle)
-        {
-            _myTransform.rotation = Quaternion.Euler(0,0,currentAngle);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -37,13 +14,14 @@ public class ClubHitboxBehaviour : WeaponBehaviour
         Knockback(collision, _myTransform);
     }
 
+    public IEnumerator DestroyMe(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
     private void Awake()
     {
         _myTransform = transform;
         
-    }
-    private void Start()
-    {
-        maxAngle = currentAngle - attackAngleRange;
     }
 }
