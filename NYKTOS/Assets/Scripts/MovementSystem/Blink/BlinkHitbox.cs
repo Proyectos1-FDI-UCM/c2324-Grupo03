c90bce1547f3efb80db79f6d55b0d01e8c0a768c
MaEduardaB <157380163+MaEduardaB@users.noreply.github.com>
2024-03-20 10:58:17 +0100

    guille cosas

--- NYKTOS/Assets/Scripts/MovementSystem/Blink/BlinkHitbox.cs
+++ NYKTOS/Assets/Scripts/MovementSystem/Blink/BlinkHitbox.cs
@@ -11,12 +11,15 @@ public class BlinkHitbox : MonoBehaviour
     private SpriteRenderer renderer;
     #endregion
 
+    List<Collider2D> numberOfColliders = new List<Collider2D>();
+
     public bool isColliding = false;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         //el numero de capa de whatLayerToDetect, es decir, TerrainCollision, es 3. OutOfBounds es 6.
         if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
+            numberOfColliders.Add(collision);
             isColliding = true;
             renderer.color = Color.red;
         }
@@ -26,9 +29,14 @@ public class BlinkHitbox : MonoBehaviour
     {
         if (collision.gameObject.layer == 3 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
-            isColliding = false;
-            renderer.color = Color.green;
+            numberOfColliders.Remove(collision);
+            if (numberOfColliders.Count == 0)
+            {
+                isColliding = false;
+                renderer.color = Color.green;
+            }
         }
+            
     }
 
     private void Awake()
