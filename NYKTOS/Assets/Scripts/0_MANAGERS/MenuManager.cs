86e94234ae0d94abb004cc10acaaeb4f8a523af3
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-03-20 10:56:05 +0100

    Cambiado MenuManager. Hay que referencia los menus de nuevo!!!!

--- NYKTOS/Assets/Scripts/0_MANAGERS/MenuManager.cs
+++ NYKTOS/Assets/Scripts/0_MANAGERS/MenuManager.cs
@@ -26,53 +26,133 @@ public class MenuManager : MonoBehaviour
 
     #region references
     [SerializeField]
-    private GameObject[] _menuList;
-
-    [SerializeField]
-    private Button[] _firstSelected;
-
-    [SerializeField]
     private GameObject _player;
 
     private PlayerController _playerController;
     private PlayerStateMachine _playerStateMachine;
+
+    #region menu refs
+    [Header("Menu References")]
+    [SerializeField]
+    private GameObject _pauseMenu;
+
+    [SerializeField]
+    private GameObject _nexusMenu;
+
+    [SerializeField]
+    private GameObject _altarMenu;
+
+    [SerializeField]
+    private GameObject _defenseMenu;
+
+    [SerializeField]
+    private GameObject _weaponUpgradeMenu;
+
+    [SerializeField]
+    private GameObject _weaponEffectMenu;
+
+    // Si los settings son solo el sonido, podemos poner un slider en el menu de pausa y ya
+    //[SerializeField] 
+    //private GameObject _settingsMenu;
     #endregion
 
-    // Menú activo actualmente. Por si hiciese falta acceder a él desde fuera
-    private int _menuId = -1;
+    #region button refs
+    [Header("Button references")]
+    [SerializeField]
+    private Button _pauseButton;
 
-    public void OpenMenu(int newId)
+    [SerializeField]
+    private Button _nexusButton;
+
+    [SerializeField]
+    private Button _altarButton;
+
+    [SerializeField]
+    private Button _defenseButton;
+
+    [SerializeField]
+    private Button _weaponUpgradeButton;
+
+    [SerializeField]
+    private Button _weaponEffectButton;
+
+    //[SerializeField]
+    //private Button _settingsButton;
+
+    #endregion
+
+    #endregion
+
+    #region properties
+    private GameObject _previousMenu;
+
+    private GameObject[] _menuList;
+    #endregion
+
+
+    #region methods
+    private void SwitchToUIControls()
     {
-        if (newId >= 0)
-        {
-            foreach (GameObject menu in _menuList) menu.SetActive(false);
-            _menuId = newId;
-            _menuList[_menuId].SetActive(true);
-            _firstSelected[_menuId].Select();
-
-            _playerStateMachine.SetState(PlayerState.OnMenu);
-            _playerController.playerControls.Player.Disable();
-            _playerController.playerControls.UI.Enable();
-            _playerController.CallMove(Vector2.zero);
-        }
+        _playerController.playerControls.Player.Disable();
+        _playerStateMachine.SetState(PlayerState.OnMenu);
+        _playerController.playerControls.UI.Enable();
+        _playerController.CallMove(Vector2.zero);
     }
-
-    public void CloseMenu()
+    private void SwitchToPlayerControls()
     {
-        foreach (GameObject menu in _menuList) menu.SetActive(false);
-        _menuId = -1;
-
         _playerStateMachine.SetState(PlayerState.Idle);
         _playerController.playerControls.UI.Disable();
         _playerController.playerControls.Player.Enable();
     }
 
-    public void CloseMenu(InputAction.CallbackContext context)
+    private void RegisterMenus()
     {
-        CloseMenu();
+        _menuList[0] = _pauseMenu;
+        _menuList[1] = _nexusMenu;
+        _menuList[2] = _altarMenu;
+        _menuList[3] = _defenseMenu;
+        _menuList[4] = _weaponUpgradeMenu;
+        _menuList[5] = _weaponEffectMenu;
+
+    }
+
+    #region open menus
+    private void OpenMenu(GameObject menu, Button button)
+    {
+        menu.SetActive(true);
+        button.Select();
+        SwitchToUIControls();
+    }
+
+    public void OpenPauseMenu() => OpenMenu(_pauseMenu, _pauseButton);
+    public void OpenNexusMenu() => OpenMenu(_nexusMenu, _nexusButton);
+    public void OpenAltarMenu() => OpenMenu(_altarMenu, _altarButton);
+    public void OpenDefense() => OpenMenu(_defenseMenu, _defenseButton);
+    public void OpenWeaponUpgradeMenu() => OpenMenu(_weaponUpgradeMenu, _weaponUpgradeButton);
+    public void OpenWeaponEffectMenu() => OpenMenu(_weaponEffectMenu, _weaponEffectButton);
+
+    #endregion
+
+    #region close menus
+    public void CloseAllMenus()
+    {
+        foreach (GameObject menu in _menuList) menu.SetActive(false);
+
+        SwitchToPlayerControls();
         GameManager.Instance.Resume();
     }
 
+    public void CloseMenu(InputAction.CallbackContext context)
+    {
+        CloseAllMenus(); 
+    }
+    #endregion
+    #endregion
+
+
+    #region oldManager
+
+    #endregion
 
     void Awake()
     {
@@ -84,6 +164,8 @@ public class MenuManager : MonoBehaviour
     {
         _playerController = _player.GetComponent<PlayerController>();
         _playerStateMachine = _player.GetComponent<PlayerStateMachine>();
+
+        RegisterMenus();
 
         _playerController.playerControls.UI.CloseMenu.performed += CloseMenu;
     }
