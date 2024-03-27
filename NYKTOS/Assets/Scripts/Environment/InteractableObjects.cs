6dbc4ffbc93d7439f101dee80c4d0b4310f6dc37
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-03-27 20:52:22 +0100

    primera version de los ismbolitos de interaccion

--- NYKTOS/Assets/Scripts/Environment/InteractableObjects.cs
+++ NYKTOS/Assets/Scripts/Environment/InteractableObjects.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractableObjects : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _sprite;
+
+    [SerializeField]
+    private GameObject _object;
+    private string _name;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponentInParent<PlayerController>() != null)
+        if (collision.gameObject.name == _name)
         {
-            Debug.Log("dentro");
-            gameObject.SetActive(true);
+            _sprite.SetActive(false);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.TryGetComponent(out PlayerController player)) gameObject.SetActive(false);
+        if (collision.gameObject.name == _name) _sprite.SetActive(true);
+    }
+
+    private void Start()
+    {
+        _name = _object.name;
+        Debug.Log(_name);
     }
 }
