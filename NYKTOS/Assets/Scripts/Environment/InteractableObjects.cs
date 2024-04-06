04b9c79b64185d7bd4aaa45fb7caafa26c450cec
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-04-06 15:13:09 +0200

    Arreglada interaccion placeholders de noche

--- NYKTOS/Assets/Scripts/Environment/InteractableObjects.cs
+++ NYKTOS/Assets/Scripts/Environment/InteractableObjects.cs
@@ -16,7 +16,12 @@ public class InteractableObjects : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == _name && _buildingState.isInteractable) _sprite.SetActive(true);
+        //if (collision.gameObject.name == _name && _buildingState.isInteractable) _sprite.SetActive(true);
+        if (collision.gameObject.name == _name && _buildingState.isInteractable)
+        {
+            _sprite.SetActive(true);
+            Debug.Log("se puede interactuar: " + _buildingState.isInteractable);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
