8bd54ff8e7cbfb799abd8865e5ecdac7e3af57a3
MaEduardaB <mbeckers@ucm.es>
2024-04-07 22:43:02 +0200

    Staff second coso

--- NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/Staff/WeaponStaff.cs
+++ NYKTOS/Assets/Scripts/AttackSystem/WeaponScripts/Staff/WeaponStaff.cs
@@ -53,7 +53,7 @@ public class WeaponStaff : Weapon
             a.SetKnockbackArea(_knockbackRadius);
         }
 
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(1f);
 
         Destroy(current);
     }
