ce8fabaf853d7f8e92384f0d6752c736e03fd1cc
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-04-08 00:23:28 +0200

    Void emitter de tutorial creado

--- NYKTOS/Assets/Scripts/Player/PlayerController.cs
+++ NYKTOS/Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour, IKnockback
