d7745abf19dc682cb190a43b3a09a51fac3649f0
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2024-04-10 10:44:12 +0200

    Avances del reloj

--- NYKTOS/Assets/Reloj.cs
+++ NYKTOS/Assets/Reloj.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Reloj : MonoBehaviour
 {
+    [SerializeField]
+    private Image _clockHand;
+
+    //clock variables
+
+    private bool _timerOn = false;
+    private float _currentTime;
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,28 @@ public class Reloj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if( _timerOn)
+        {
+            ChangeTime();
+        }
+    }
+
+    private void ChangeTime()
+    {
+        _currentTime -= Time.deltaTime;
+
+        if(_currentTime < 0)
+        {
+            _timerOn = false;
+        }
+    }
+
+    public void ActivateTimer(float maxTime)
+    {
+        _currentTime = maxTime;
+        _timerOn = true;
         
     }
+
+
 }
