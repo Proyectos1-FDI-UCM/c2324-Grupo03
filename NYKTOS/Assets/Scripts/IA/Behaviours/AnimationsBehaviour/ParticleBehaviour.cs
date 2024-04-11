01a47642737cf54b861dde235141f2a17fd47284
MaEduardaB <mbeckers@ucm.es>
2024-04-11 19:26:38 +0200

    los enemigos se ponen blancos

--- NYKTOS/Assets/Scripts/IA/Behaviours/AnimationsBehaviour/ParticleBehaviour.cs
+++ NYKTOS/Assets/Scripts/IA/Behaviours/AnimationsBehaviour/ParticleBehaviour.cs
@@ -7,12 +7,16 @@ public class ParticleBehaviour : MonoBehaviour, IBehaviour {
     /// <summary>
     /// Cambio del material de la particulas cuando sea atacado
     /// </summary>
+ 
+    private SpriteRenderer _spriteRenderer;
+
     [Header("Materials")]
     [SerializeField]
-    private Material _materialToChange;
-
-    private Material _previousMaterial;
-
+    private Material _particleMaterialToChange;
+    private Material _previousParticleMaterial;
+    [SerializeField]
+    private Material _spriteMaterialToChange;
+    private Material _previousSpriteMaterial;
 
     [SerializeField]
     private ParticleSystem explosionParticleSystem;
@@ -22,6 +26,12 @@ public class ParticleBehaviour : MonoBehaviour, IBehaviour {
     private float _emisionNormal = 10f;
     [SerializeField]
     private float _speedNormal = 1f;
+    [SerializeField]
+    private float _sizeNormal = 10f;
+    
+    [Header("Size")]
+    [SerializeField]
+    private float _sizeExplosion = 1f;
 
 
     [Header("Changes")]
@@ -40,31 +50,38 @@ public class ParticleBehaviour : MonoBehaviour, IBehaviour {
 
     }
     private void Start() {
-        _previousMaterial = GetComponentInParent<HealthComponent>().GetComponentInChildren<ParticleSystemRenderer>().material;
+        _previousParticleMaterial = GetComponentInParent<HealthComponent>().GetComponentInChildren<ParticleSystemRenderer>().material;
+        _spriteRenderer = GetComponentInParent<HealthComponent>().GetComponentInChildren<SpriteRenderer>();
+        _previousSpriteMaterial = _spriteRenderer.material;
     }
 
     private IEnumerator ChangeParticles() 
     {
 
         ParticleSystemRenderer settings = GetComponentInParent<HealthComponent>().GetComponentInChildren<ParticleSystemRenderer>();
-        settings.material = _materialToChange;
+        settings.material = _particleMaterialToChange;
+
+        
+        _spriteRenderer.material = _spriteMaterialToChange;
 
         var mainModule = explosionParticleSystem.main;
         mainModule.startSpeed = new ParticleSystem.MinMaxCurve(_speedInicial, _speedFinal);
 
+        mainModule.startSize = _sizeExplosion;
 
         var emissionModule = explosionParticleSystem.emission;
         emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(_emisionInicial, _emisionFinal);
 
        
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.6f);
 
-        
+        mainModule.startSize = _sizeNormal;
         mainModule.startSpeed = new ParticleSystem.MinMaxCurve(_speedNormal);
 
         emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(_emisionNormal);
 
+        _spriteRenderer.material = _previousSpriteMaterial;
 
-        settings.material = _previousMaterial;
+        settings.material = _previousParticleMaterial;
     }
 }
