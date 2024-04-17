dcdde2d174a201c15cf4cdceb18bfc2f735a1627
MaEduardaB <mbeckers@ucm.es>
2024-04-17 20:42:02 +0200

    mappaaaaaaaaaaaaaaaaaaaa

--- NYKTOS/Assets/Scripts/UI/MiniMap.cs
+++ NYKTOS/Assets/Scripts/UI/MiniMap.cs
@@ -1,56 +1,45 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class MiniMap : MonoBehaviour
-{
-	//Se encarga de convertir la posicion del player para el mapa
-	[Header("Transforms World")]
-	[SerializeField] private Transform _playerTransform;
-	[SerializeField] private Transform _point1;
-	[SerializeField] private Transform _point2;
+public class MiniMap : MonoBehaviour {
+    //Se encarga de convertir la posicion del player para el mapa
+    [Header("Transforms World")]
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _point1;
+    [SerializeField] private Transform _point2;
 
-	[Header ("Transforms MiniMap")]
-	[SerializeField] private RectTransform _playerTransformRect;
-	[SerializeField] private RectTransform _point1Rect;
-	[SerializeField] private RectTransform _point2Rect;
+    [Header("Transforms MiniMap")]
+    [SerializeField] private RectTransform _playerMiniMap;
+    [SerializeField] private RectTransform _miniMapPoint1;
+    [SerializeField] private RectTransform _miniMapPoint2;
 
-	private float miniMapRatio =0f;
+    private float miniMapRatio = 0f;
 
-	void Awake()
-	{
-		MiniMapRatio();
-	}
+    void Awake() {
+        MiniMapRatio();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		Debug.Log(_playerTransform.position.x);
-		_playerTransformRect.anchoredPosition = _point1Rect.anchoredPosition + new Vector2((_playerTransform.position.x - _point1.position.x) *miniMapRatio , (_playerTransform.position.y - _point1.position.y)*miniMapRatio);
-	}
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(_playerTransform.position.x);
+        _playerMiniMap.anchoredPosition = _miniMapPoint1.anchoredPosition + new Vector2((_playerTransform.position.x - _point1.position.x) * miniMapRatio, (_playerTransform.position.y - _point1.position.y) * miniMapRatio);
+    }
 
 
-	private void MiniMapRatio()
-	{
-		//Distancia en el mundo
-		Vector3 vecDistancia = _point1.position - _point2.position;
-		//vecDistancia.y = 0f;
-		float distanciaWorld = vecDistancia.magnitude;
-		/*distanciaWorld = Mathf.Sqrt(
-			Mathf.Pow(_point1.position.x - _point2.position.x, 2) +
-			Mathf.Pow(_point1.position.y - _point2.position.y, 2));*/
+    private void MiniMapRatio() {
+
+        //Distancia en el mundo
+        Vector3 vecDistancia = _point1.position - _point2.position;
+        vecDistancia.y = 0f;
+        float distanciaWorld = vecDistancia.magnitude;
+
+        // distancia MiniMap
+        
+        float distanciaMiniMap = (_miniMapPoint1.anchoredPosition -_miniMapPoint2.anchoredPosition).magnitude;
+        miniMapRatio = distanciaMiniMap / distanciaWorld;
+        Debug.Log(miniMapRatio);
 
 
-		//Distancia en el MiniMap
-
-		Vector3 vecdistanciaMiniMap = _point1Rect.anchoredPosition - _point2Rect.anchoredPosition;
-		float distanciaMiniMap = Mathf.Sqrt(
-			Mathf.Pow(_point1Rect.anchoredPosition.x - _point2Rect.anchoredPosition.x, 2) +
-			Mathf.Pow(_point1Rect.anchoredPosition.y - _point2Rect.anchoredPosition.y, 2));
-
-		miniMapRatio = distanciaWorld - distanciaMiniMap;
-		Debug.Log(miniMapRatio);
-
-
-	}
+    }
 
 }
