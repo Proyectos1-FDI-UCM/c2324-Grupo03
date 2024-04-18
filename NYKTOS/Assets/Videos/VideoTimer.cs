970ad41aefe9c2516814e6e7ad35f241b777f6a2
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2024-04-18 23:14:15 +0200

    El vídeo se activa por un evento

--- NYKTOS/Assets/Videos/VideoTimer.cs
+++ NYKTOS/Assets/Videos/VideoTimer.cs
@@ -6,20 +6,36 @@ using UnityEngine.Video;
 
 public class VideoTimer : MonoBehaviour
 {
+    [SerializeField]
+    private VoidEmitter _start;
+
     public VideoPlayer video;
     
     
     private void Awake()
     {
         video = GetComponent<VideoPlayer>();
-        
+
+        _start.Perform.AddListener(StartVideo);
         
         video.loopPointReached += End;
         
     }
-     void End (VideoPlayer vp) 
+
+    private void StartVideo()
+    {
+        video.Play();
+    }
+
+    void End (VideoPlayer vp) 
     {
         video.Pause();
         
+    }
+
+    private void OnDestroy()
+    {
+        _start.Perform.RemoveListener(StartVideo);
+        video.loopPointReached -= End;
     }
 }
