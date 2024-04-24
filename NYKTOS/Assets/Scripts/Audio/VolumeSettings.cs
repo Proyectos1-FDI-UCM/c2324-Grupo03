aff75cefaad12ff523adaca9a4df8fd8e67a38b9
MaEduardaB <157380163+MaEduardaB@users.noreply.github.com>
2024-04-24 10:57:32 +0200

    comentarios scripts

--- NYKTOS/Assets/Scripts/Audio/VolumeSettings.cs
+++ NYKTOS/Assets/Scripts/Audio/VolumeSettings.cs
@@ -1,51 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+/// <summary>
+/// Clase responsable del ajuste de volumen
+/// </summary>
 public class VolumeSettings : MonoBehaviour
 {
-    //Configuracion del sonido de Maria
+	#region references
+	[SerializeField]
+	private AudioMixer _audioMixer;
+	#endregion
 
-    [SerializeField]
-    private AudioMixer _audioMixer;
+	private void Start()
+	{
+		SetVolume(0.5f, VolumeType.Master);
+		SetVolume(0.5f, VolumeType.Effects);
+		SetVolume(0.5f, VolumeType.Music);
+	}
 
-    private void Start() {
-        SetVolume(0.5f, VolumeType.Master);
-        SetVolume(0.5f, VolumeType.Effects);
-        SetVolume(0.5f, VolumeType.Music);
-    }
-    public void SetMasterVolumeSlider(float volume) {
-        SetVolume(volume, VolumeType.Master);
-    }
+	/// <summary>
+	/// Método para cambiar el volumen del master, es llamado por el slider General del SettingsMenu
+	/// </summary>
+	/// <param name="volume"> parametro float que se recibe del slider del Settings Menu </param>
+	public void SetMasterVolumeSlider(float volume)
+	{
+		SetVolume(volume, VolumeType.Master);
+	}
 
-    public void SetEffectsVolumeSlider(float volume) {
-        SetVolume(volume, VolumeType.Effects);
-    }
+	/// <summary>
+	/// Método para cambiar el volumen de los efectos, es llamado por el slider SFX del SettingsMenu
+	/// </summary>
+	/// <param name="volume"> parametro float que se recibe del slider del Settings Menu </param>
+	public void SetEffectsVolumeSlider(float volume)
+	{
+		SetVolume(volume, VolumeType.Effects);
+	}
+	/// <summary>
+	/// Método para cambiar el volumen de la musica, es llamado por el slider Musica del SettingsMenu
+	/// </summary>
+	/// <param name="volume"> parametro float que se recibe del slider del Settings Menu </param>
+	public void SetMusicVolumeSlider(float volume)
+	{
+		SetVolume(volume, VolumeType.Music);
+	}
 
-    public void SetMusicVolumeSlider(float volume) {
-        SetVolume(volume, VolumeType.Music);
-    }
-    public void SetVolume(float volume, VolumeType type) {
-        switch (type) {
-            case VolumeType.Master:
-                _audioMixer.SetFloat("Volume", Mathf.Log10(volume*2f)*20f);
-                
-                break;
-            case VolumeType.Effects:
-                _audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume*2f)*20f);
-                
-                break;
-            case VolumeType.Music:
-                _audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume *2f) * 20f);
-                break;
-        }
-    }
+	/// <summary>
+	/// Método que cambia el volumen en función del tipo del audio
+	/// </summary>
+	/// <param name="volume"> parametro float que se recibe del slider del Settings Menu </param>
+	/// <param name="type"> el tipo de volumen (Master, Effects o Music) que estan definidos por el Enum VolumeType </param>
+	public void SetVolume(float volume, VolumeType type)
+	{
+		switch (type)
+		{
+			case VolumeType.Master:
+				_audioMixer.SetFloat("Volume", Mathf.Log10(volume * 2f) * 20f);
+
+				break;
+			case VolumeType.Effects:
+				_audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume * 2f) * 20f);
+
+				break;
+			case VolumeType.Music:
+				_audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume * 2f) * 20f);
+				break;
+		}
+	}
 }
-public enum VolumeType {
-    Master,
-    Effects,
-    Music
+
+/// <summary>
+/// Enum de los distintos tipos de audio
+/// </summary>
+public enum VolumeType
+{
+	Master, // Volumen general
+	Effects,
+	Music
 }
 
 
--- NYKTOS/Assets/Scripts/Environment/Altar/ChangeSpritesByEvents.cs
+++ NYKTOS/Assets/Scripts/Environment/Altar/ChangeSpritesByEvents.cs
@@ -1,63 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+/// <summary>
+/// Clase responsable de cambiar los sprites de los altares en función de su estado
+/// </summary>
 [RequireComponent(typeof(SpriteRenderer))]
-public class ChangeSpritesByEvents : MonoBehaviour
+public class ChangeSpritesAltares : MonoBehaviour
 {
+	#region references
+	[SerializeField]
+	private VoidEmitter _placeHolderSprite;
 
-    [SerializeField] private VoidEmitter _placeHolderSprite;
+	private BuildingStateMachine _state;
+	private AltarComponent _altarComponent;
+	private SpriteRenderer _currentSprite;
 
-    private BuildingStateMachine _state;
-    private AltarComponent _altarComponent;
-    private SpriteRenderer _currentSprite;
+	#region sprites
+	[Header("Sprites")]
+	[Space(10)]
+	[Header("    Destruido")]
+	[SerializeField]
+	private Sprite _sprite0;
 
-    [SerializeField]
-    private Sprite _sprite0;
-    [SerializeField]
-    private Sprite _sprite1;
-    [SerializeField]
-    private Sprite _sprite2;
-    [SerializeField]
-    private Sprite _sprite3;
-    [SerializeField]
-    private Sprite _sprite4;
+	[Space(10)]
+	[Header("    Casi Destruido")]
+	[SerializeField]
+	private Sprite _sprite1;
 
-    void Start()
-    {
-        _currentSprite = GetComponent<SpriteRenderer>();
+	[Space(10)]
+	[Header("    Medio Construido")]
+	[SerializeField]
+	private Sprite _sprite2;
 
-        _altarComponent = GetComponentInParent<AltarComponent>();
-        _placeHolderSprite.Perform.AddListener(ChangeSprite);
-        _state = GetComponentInParent<BuildingStateMachine>();
+	[Space(10)]
+	[Header("    Casi Construido")]
+	[SerializeField]
+	private Sprite _sprite3;
 
-        ChangeSprite();
-    }
+	[Space(10)]
+	[Header("    Construido")]
+	[SerializeField]
+	private Sprite _sprite4;
+	#endregion
 
-    private void ChangeSprite() 
-    {
-        if (_state.buildingState == BuildingStateMachine.BuildingState.Built) {
-            _currentSprite.sprite = _sprite4;
-        } else{
+	#endregion
 
-            switch (_altarComponent.CurrentPlaceholders) {
-                case 0:
-                    _currentSprite.sprite = _sprite0;
-                    break;
-                case 1:
-                    _currentSprite.sprite = _sprite1;
-                    break;
-                case 2:
-                    _currentSprite.sprite = _sprite2;
-                    break;
-                case 3:
-                    _currentSprite.sprite = _sprite3;
-                    break;
-            }
+	void Start()
+	{
+		_currentSprite = GetComponent<SpriteRenderer>();
+		_altarComponent = GetComponentInParent<AltarComponent>();
+		_placeHolderSprite.Perform.AddListener(ChangeSprite);
+		_state = GetComponentInParent<BuildingStateMachine>();
 
-        }
-    }
+		ChangeSprite();
+	}
 
-    private void OnDestroy() {
-        _placeHolderSprite.Perform.RemoveListener(ChangeSprite);
-    }
+	/// <summary>
+	/// Método responsable de cambiar los sprites de los altares
+	/// </summary>
+	private void ChangeSprite()
+	{
+		if (_state.buildingState == BuildingStateMachine.BuildingState.Built)
+		{
+			_currentSprite.sprite = _sprite4;
+		}
+		else
+		{
+			switch (_altarComponent.CurrentPlaceholders)
+			{
+				case 0:
+					_currentSprite.sprite = _sprite0;
+					break;
+				case 1:
+					_currentSprite.sprite = _sprite1;
+					break;
+				case 2:
+					_currentSprite.sprite = _sprite2;
+					break;
+				case 3:
+					_currentSprite.sprite = _sprite3;
+					break;
+			}
+		}
+	}
+	/// <summary>
+	/// Método para eliminar el listener
+	/// </summary>
+	private void OnDestroy()
+	{
+		_placeHolderSprite.Perform.RemoveListener(ChangeSprite);
+	}
 }
