932e28f519a905bd4fde9388479ee9a092eb9832
Marco <marcgo19@ucm.es>
2024-04-25 17:40:06 +0200

    Añadido opcion de debug para los emitters

--- NYKTOS/Assets/Scripts/Emitters/GenericEmitter.cs
+++ NYKTOS/Assets/Scripts/Emitters/GenericEmitter.cs
@@ -24,12 +24,19 @@ public abstract class GenericEmitter<T> : ScriptableObject
 public abstract class GenericEmitter : ScriptableObject
 {
     [SerializeField]
+    private bool debug = false;
+
+    [SerializeField]
     private UnityEvent _perform = new UnityEvent();
     public UnityEvent Perform{get{return _perform;}}
 
     public void InvokePerform()
     {
-        Debug.LogError("[EMITTER] (" + name + ") Emitter lanzado");
+        if (debug) 
+        {
+            Debug.LogError("[EMITTER] (" + name + ") Emitter lanzado");
+        }
+
         _perform.Invoke();
     }
 }
