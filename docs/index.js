6bddb0a1c5e760c66c60180839911c9a31bb0a0c
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-05-21 20:05:10 +0200

    Ahora si header chulardo

---
+++ docs/index.js
@@ -0,0 +1,12 @@
+
+function ToggleActiveClass(id) {
+    document.getElementById(id).classList.toggle('active');
+}
+
+function AddActiveClass(id) {
+    document.getElementById(id).classList.add('active');
+}
+
+function RemoveActiveClass(id) {
+    document.getElementById(id).classList.remove('active');
+}
